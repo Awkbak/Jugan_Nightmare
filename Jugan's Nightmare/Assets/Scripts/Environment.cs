038aca76fdfdc63be132c6a384c3798bca603037
Murilo <mcosta1@uncc.edu>
2015-09-11 22:45:45 -0400

    ggez

---
+++ Jugan's Nightmare/Assets/Scripts/Environment.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Environment : MonoBehaviour {
+    public bool player_turn = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Jugan's Nightmare/Assets/Scripts/Jugan_Thinks.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jugan_Thinks : MonoBehaviour {
+    public int difficulty;
+    var target : 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if()
+	}
+}
