212120dfadb349e0b3ca628ac7aeac09363d6c4e
Kirsti <camlin@carolina.rr.com>
2015-09-24 18:05:02 -0400

    Select nodes

--- Jugan's Nightmare/Assets/Scripts/Player.cs
+++ Jugan's Nightmare/Assets/Scripts/Player.cs
@@ -118,6 +118,27 @@ public class Player : MonoBehaviour {
 
     public void moving()
     {
- 
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+            Transform choose = GameObject.FindWithTag("select").transform;
+
+            if (Physics.Raycast (ray, out hit, 100.0f))
+            {
+                if (hit.transform.gameObject.tag == "select")
+                {
+                    Debug.Log ("It's working!");
+                } else {
+                    Debug.Log ("nopz");
+                }
+
+            }
+        }
+    }
+
+    void Update()
+    {
+        moving();
     }
 }
